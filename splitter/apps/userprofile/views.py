87a23f5e88f78ef9ab2deab407496ab7b85a25c2
Christoffer Nysæther <christoffer.nysaether@gmail.com>
2017-09-16 20:25:06 +0200

    Update gitignore and testview

--- splitter/apps/userprofile/views.py
+++ splitter/apps/userprofile/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from splitter.apps.api import dnb as dnb_api
 from splitter.apps.controller.splitcontrol import SplitterController
+from .models import UserProfile, Transaction
+from splitter.apps.group.models import Group
 
 def index(request):
-    user = request.user
-    # accounts = dnb_api.get_accounts('07066363656')
     ctr = SplitterController()
+    cid = '23088983723'
+    user = UserProfile.objects.get(customer_id=cid)
+
+    main_user_transactions = ctr.get_user_transactions(user_id=user.customer_id)
+    # accounts = dnb_api.get_accounts('07066363656')
     someone = ctr.get_user(user_id='07066363656')
-    return HttpResponse("<h3>{0}</h3>".format(someone))
+    return HttpResponse("<h3>{0}</h3>".format(user.user.get_full_name()))
 
     #TODO: Get necessary information to view

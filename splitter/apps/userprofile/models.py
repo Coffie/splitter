15c5d9f8f538f15b392da9586110fd4c147bf172
Christoffer Nysæther <christoffer.nysaether@gmail.com>
2017-09-17 01:04:34 +0200

    Add latest transactions to index and fix controller

--- splitter/apps/userprofile/models.py
+++ splitter/apps/userprofile/models.py
@@ -3,19 +3,19 @@ from django.contrib.auth.models import User
 from splitter.apps.group.models import Group
 
 class UserProfile(models.Model):
-    customer_id = models.BigIntegerField(primary_key=True)
+    customer_id = models.CharField(primary_key=True, max_length=32)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    account_id = models.BigIntegerField()
+    account_id = models.CharField(max_length=32)
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user.get_full_name()
     
 class Transaction(models.Model):
-    transaction_id = models.BigIntegerField(primary_key=True)
+    transaction_id = models.CharField(primary_key=True, max_length=32)
     customer = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     timestamp = models.DateTimeField()
-    amount = models.BigIntegerField()
+    amount = models.CharField(max_length=32)
     description = models.CharField(max_length=200)
     relevant = models.BooleanField(default=False)
 

05e07f549e6ede4ee4738bb655c607ec95dc79cc
Christoffer Nysæther <christoffer.nysaether@gmail.com>
2017-09-17 02:17:12 +0200

    Add content to main view

---
+++ splitter/apps/userprofile/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+class PaymentForm(forms.Form):
+    amount = forms.CharField(max_length=10)
+    message = forms.CharField(max_length=64)
+    
+    def clean(self):
+        cleaned_data = super(ContactForm, self).clean()
+        amount = cleaned_data.get('amount')
+        message = cleaned_data.get('message')
+        if not amount or not message:
+            raise forms.ValidationError('Please fill in both fields')
+
+
--- splitter/apps/userprofile/views.py
+++ splitter/apps/userprofile/views.py
@@ -4,6 +4,7 @@ from splitter.apps.api import dnb as dnb_api
 from splitter.apps.controller.splitcontrol import SplitterController
 from .models import UserProfile, Transaction
 from splitter.apps.group.models import Group
+from .forms import PaymentForm
 
 def index(request):
     ctr = SplitterController()
@@ -22,20 +23,56 @@ def index(request):
         desc += transaction.description[:14] + "..."
         transaction.description = desc
 
-    ctr.update_data()
+    # ctr.update_data()
     main_user_transactions = ctr.get_user_transactions(user_id=user.customer_id)
     # accounts = dnb_api.get_accounts('07066363656')
     someone = ctr.get_user(user_id='07066363656')
+    balance = ctr.get_primary_account_balance(user_id=user.customer_id)
 
-    context = {
-            'transactions': transactions,
-            'group': group,
-            'group_transactions': group_transactions,
-            'user': user,
-            }
 
-    return render(request, 'userprofile/index.html', context)
+    if request.method == 'POST':
+        form = PaymentForm(request.POST)
+        if form.is_valid():
+            amount = request.POST['amount']
+            message = request.POST['description']
+            ctr.make_card_payment(user_id=user.customer_id, amount=amount, message=message)
+            transactions = ctr.get_user_transactions(user_id=user.customer_id)[-9:]
+            transactions = list(reversed(transaction))
+            for transaction in transactions:
+                desc = ""
+                desc += transaction.description[:14] + "..."
+                transaction.description = desc
+            balance = ctr.get_primary_account_balance(user_id=user.customer_id)
+            
+            context = {
+                    'transactions': transactions,
+                    'group': group,
+                    'user': user,
+                    'balance': balance,
+                    'form': form,
+                    }
+            return render(request, 'userprofile/index.html', context)
+                    
+
+    else:
+        form = PaymentForm()
+        context = {
+                'transactions': transactions,
+                'group': group,
+                'group_transactions': group_transactions,
+                'user': user,
+                'balance': balance,
+                'form': form,
+                }
+
+        return render(request, 'userprofile/index.html', context)
 
     
+def make_message_readable(transactions):
+    text = []
+    for transaction in transactions:
+        desc = ""
+        desc += transaction.description[:14] + "..."
+        transaction.description = desc
+    return transactions
 
-    #TODO: Get necessary information to view
